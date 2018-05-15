5b0ce300a28ef956d4d593cbb775f4b383c57141
LAPTOP-1G8Q22EQ\Julien <jujuver96@gmail.com>
2018-05-15 17:01:35 +0200

    Version 1.1

---
+++ Aeroglide/Iteration 4/Aeroglide/src/be/iesca/aeroglide/test/TestNG_PiloteDao.java
@@ -0,0 +1,74 @@
+package be.iesca.aeroglide.test;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import java.util.List;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import be.iesca.aeroglide.dao.PiloteDao;
+import be.iesca.aeroglide.domaine.Pilote;
+
+public abstract class TestNG_PiloteDao {
+	protected PiloteDao piloteDao;
+	private Pilote p1;
+	private Pilote p2;
+	private Pilote p3;
+	private Pilote p4;
+
+	@BeforeClass
+	public abstract void init();
+	
+	@Test
+	public void testEnregistrerPilote()
+	{
+		//	L'ajout des pilotes sera réussi
+		this.p1=new Pilote("A", "A", "aa@gmail.com", "A", "1", "A", 1234, "123/12.34.56", 1);
+		this.p2=new Pilote("B", "B", "bb@gmail.com", "B", "2", "B", 4567, "987/65.43.21", 2);
+		this.p3=new Pilote("C", "B", "cc@gmail.com", "C", "3", "C", 7894, "025/78.21.65", -250);
+		this.p4=new Pilote("D", "D", "dd@gmail.com", "D", "4", "D", 5874, "087/85.23.54", -500);
+		
+		try{
+			assertTrue(this.piloteDao.ajouterPilote(p1));
+			assertTrue(this.piloteDao.ajouterPilote(p2));
+			assertTrue(this.piloteDao.ajouterPilote(p3));
+			assertTrue(this.piloteDao.ajouterPilote(p4));
+		}catch(Exception e){
+			fail("L'ajout aurait dû réussir");
+		}
+	}
+	
+	@Test(dependsOnMethods="testEnregistrerPilote")
+	public void testListerPilotes(){
+		List<Pilote> liste = this.piloteDao.listerPilotes();
+		
+		assertTrue(liste.get(0).equals(p1));
+		assertTrue(liste.get(1).equals(p2));
+		assertTrue(liste.get(2).equals(p3));
+		assertTrue(liste.get(3).equals(p4));
+	}
+
+	@Test(dependsOnMethods = "testEnregistrerPilote")
+	public void testListerPilotesSoldeNegatif(){
+		List<Pilote> liste = this.piloteDao.listerPilotesSoldeNegatif();
+
+		assertTrue(liste.get(0).equals(p4));
+		assertTrue(liste.get(1).equals(p3));
+	}
+
+	@Test(dependsOnMethods = "testListerPilotes")
+	public void testModifierPilote(){
+		Pilote p1Modifier = p1;
+		p1Modifier.setNoGsm("587/54.32.52");
+
+		assertTrue(this.piloteDao.modifierPilote(p1Modifier));
+
+		Pilote p2Modifier = p2;
+		p2Modifier.setLocalite("Bruxelles");
+
+		assertTrue(this.piloteDao.modifierPilote(p2Modifier));
+	}
+}
---
+++ Aeroglide/Iteration 4/Aeroglide/src/be/iesca/aeroglide/usecaseimpl/GestionVolsImpl.java
@@ -0,0 +1,93 @@
+package be.iesca.aeroglide.usecaseimpl;
+
+import java.sql.Date;
+import java.util.List;
+
+import be.iesca.aeroglide.dao.VolDao;
+import be.iesca.aeroglide.daoimpl.DaoFactory;
+import be.iesca.aeroglide.domaine.Bundle;
+import be.iesca.aeroglide.domaine.TypePlaneur;
+import be.iesca.aeroglide.domaine.Vol;
+import be.iesca.aeroglide.usecase.GestionVols;
+
+public class GestionVolsImpl implements GestionVols {
+    private VolDao volDao;
+
+    public GestionVolsImpl(){
+        this.volDao = (VolDao) DaoFactory.getInstance().getDaoImpl(VolDao.class);
+    }
+
+    @Override
+    public void enregistrerVol(Bundle bundle) {
+    	Vol vol=(Vol) bundle.get(Bundle.VOL);
+    	String message="";
+    	boolean reussite=false;
+    	if(vol==null){
+    		message="Ajout échoué : aucun vol n'a �t� sp�cifi�";
+    	}else if(vol.getCout()<=0){
+    		message="Ajout échoué : le co�t ne peut pas �tre inf�rieur � 0";
+    	}else if(vol.getDuree()<=0){
+    		message="Ajout échoué : la dur�e ne peut pas �tre inf�rieure � 0";
+    	}else if(vol.getPilote()==null){
+    		message="Ajout échoué : aucun pilote n'a �t� s�lectionn�";
+    	}else if(vol.getPlaneur()==null){
+    		message="Ajout échoué : aucun planeur n'a �t� s�lectionn�";
+    	}else{
+    		double solde=vol.getPilote().getSolde();
+    		solde-=vol.getCout();
+    		vol.getPilote().setSolde(solde);
+    		reussite=this.volDao.enregistrerVol(vol);
+    		if(reussite){
+    			message="Ajout effectué avec succès";
+    		}else{
+    			message="Ajout échoué";
+    		}
+    	}
+    	
+    	bundle.put(Bundle.MESSAGE,message);
+    	bundle.put(Bundle.OPERATION_REUSSIE,reussite);
+    }
+
+    @Override
+    public void listerTypePlaneur(Bundle bundle) {
+		List<TypePlaneur> listeTypePlaneur = this.volDao.listerTypePlaneur();
+		bundle.put(Bundle.LISTE, listeTypePlaneur);
+    }
+
+	@Override
+	public void listerVols(Bundle bundle) {
+		Date date = (Date) bundle.get(Bundle.DATE);
+		String message = "";
+		boolean listeOk = false;
+		List<Vol> listeVols = null;
+
+		if(date == null){
+			message = "Échec : aucune date n'a été sélectionnée";
+			listeOk = false;
+		}else{
+			listeVols = this.volDao.listerVols(date);
+			if(listeVols == null){
+				listeOk = false;
+			}else if(listeVols.isEmpty()){
+				listeOk = false;
+				message = "Liste vide";
+			}else if(listeVols.size() == 1){
+				listeOk = true;
+				message = "Il y a un vol";
+			}else{
+				listeOk = true;
+				message = "Il y a " + listeVols.size() + " vols";
+			}
+		}
+
+		bundle.put(Bundle.LISTE, listeVols);
+		bundle.put(Bundle.OPERATION_REUSSIE, listeOk);
+		bundle.put(Bundle.MESSAGE, message);
+	}
+
+	@Override
+	public void listerDates(Bundle bundle) {
+		List<Date> listeDates = this.volDao.listerDates();
+		bundle.put(Bundle.LISTE, listeDates);
+	}
+}
